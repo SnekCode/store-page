d2296d593dc0c5d09dfd083c7ee243770074af85
SnekCode <code.snakes@gmail.com>
2021-04-28 21:05:13 -1000

    Updates

---
+++ precache-manifest.fdae71f9625234286e207dbefb8304b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "532c08fca9ce1aefd07f4b81668aee99",
+    "url": "/store-page/index.html"
+  },
+  {
+    "revision": "1644075134f1e9ac9abd",
+    "url": "/store-page/static/css/main.dd59d175.chunk.css"
+  },
+  {
+    "revision": "ef6b13f8a3c73c54f762",
+    "url": "/store-page/static/js/2.33fc0fd2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/store-page/static/js/2.33fc0fd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1644075134f1e9ac9abd",
+    "url": "/store-page/static/js/main.5d475639.chunk.js"
+  },
+  {
+    "revision": "bc841ab7501522b9e947",
+    "url": "/store-page/static/js/runtime-main.ed2e7f74.js"
+  },
+  {
+    "revision": "d69d0f0d95235f592c26cf1b9e796ab7",
+    "url": "/store-page/static/media/background.d69d0f0d.jpg"
+  }
+]);
